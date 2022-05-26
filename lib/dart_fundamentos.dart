a741e636d546af8b594e13efa75b6c27d6f4f059
Eudes Lima <eudesadvento@gmail.com>
2022-05-26 09:29:51 -0300

    Conceitos da variaveis

---
+++ lib/dart_fundamentos.dart
@@ -0,0 +1,31 @@
+// Temos que entender PRIMEIRO o conceitos de Variaveis. Elas formadas  quando armazenamos algum
+// Valores.
+// existe dois tipos:
+//  INTEIROS; Ex:. 1,2,3, 07777...
+//  DECIMAIS: EX:. 1.2 , 1,3 ...
+// temos também os textos.
+
+void main() {
+  // exite uma estrutura dentro das variaveis.
+  // int é o TIPO
+  // idade é o NOME
+  // 39 é ATRIBUIÇÃO DE VALOR
+
+  int idade = 39;
+  double valorDocarro = 25.000;
+  String Nome = 'Eudes lima';
+
+  // int: usado para números inteiros
+  // double: usado para númeradores decimais
+  // string: para textos
+
+  
+  // Não é preciso fazer a declação no (TIPO), pois na atribuição já mostra isso.
+  // Então vejamos como podemos fazer
+  // vamos usar a variavel (var)
+  // chamos isso de inferir 
+
+  var idade2 = 39;
+  var valorDocarro2 = 25.000;
+  var Nome2 = 'Eudes lima';
+}
