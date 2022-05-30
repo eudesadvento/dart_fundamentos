b10969d5299a1395cb358eac1db45037f595415b
Eudes Lima <eudesadvento@gmail.com>
2022-05-30 11:48:11 -0300

    Condicionais e Relacionais

---
+++ lib/modificarodes/Operadores_condicionais.dart
@@ -0,0 +1,18 @@
+import 'dart:html';
+
+void main() {
+  // Codicionais, são as regras ne negocios.
+  // É quando se quer emplementar regras de negocio dentro a aplicação.
+
+  final idade = 35;
+  if (idade > 35) {
+    print('pode entrar ');
+
+  } 
+  else if (idade == 35) {
+    print('pode sim entar ');
+  } 
+  else {
+    print('não pode entar ');
+  }
+}
---
+++ lib/modificarodes/operadores_relacionais.dart
@@ -0,0 +1,21 @@
+void main() {
+// temos 6 tipos
+//== (Igualdade), !=(difernça), > ( Maior que), <= (Menor Igual), >= (Maior Igual)
+// Vamos ao Ex:.
+
+  final idade = 20;
+  final tipoDeCarro = 'honda';
+
+  if (idade == 20) {
+    print('habilitado');
+  }
+  if (idade > 18) {
+    print('habilitado');
+  }
+  if (idade >= 20) {
+    print('habilitado');
+  }
+  if (tipoDeCarro != 'fiat') {
+    print('perdão não atende a regra');
+  }
+}
