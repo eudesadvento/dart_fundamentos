447d57c9c65dba17fbd4c4f79da513e5de54d8b7
Eudes Lima <eudesadvento@gmail.com>
2022-06-14 14:50:08 -0300

    Manipulando strings

---
+++ lib/FUNDAMENTOS/Conversões e manipulações.dart
@@ -0,0 +1,14 @@
+void main() {
+  final nome = "Eudes Lima";
+  var subStringNome = nome.substring(5);
+  print(subStringNome);
+  // podemos fazer de outra forma
+
+  var subStringNome2 = nome.substring(0, 5);
+  print(subStringNome2);
+
+  
+
+
+
+}
